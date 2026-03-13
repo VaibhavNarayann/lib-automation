a9701932611636a37e9d3a2a1be3c90fffa23173
VaibhavOrbit <vaibhav.readme@gmail.com>
2026-03-13 17:36:37 +0000

    created basic roadmap for server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express" 
+const app = express(); 
+
+// Enable JSON parsing
+app.use(express.json())
+
+//google steets api 
+// user-> frontend -> registration form -> user -> data -> handle to that data > node endpoint /// where a palce where we connect over data -> google sheets -> 
+//
+// summary 
+//  backend is noting but a bridge between connection to data  and google sheets 
+//
+//  app.
+//
+//
+//
+//
+//
+//
+
+	app.post(res,req()=>{
+		const data = res.body(); 
+			
+	})
