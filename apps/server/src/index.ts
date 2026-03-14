214861cb852614f9fa7935033c9ec70977a171fe
VaibhavOrbit <vaibhav.readme@gmail.com>
2026-03-14 05:30:49 +0000

    created read endpoint

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,24 +1,40 @@
 import express from "express" 
 const app = express(); 
+import {google} from "googleapis"	
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Enable JSON parsing
 app.use(express.json())
 
-//google steets api 
-// user-> frontend -> registration form -> user -> data -> handle to that data > node endpoint /// where a palce where we connect over data -> google sheets -> 
-//
-// summary 
-//  backend is noting but a bridge between connection to data  and google sheets 
-//
-//  app.
-//
-//
-//
-//
-//
-//
+const credentials  = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON!);
 
-	app.post(res,req()=>{
-		const data = res.body(); 
-			
-	})
+
+app.get("/", async (req,res)=> {
+	const auth = new google.auth.GoogleAuth({
+	credentials,
+	scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+}); 
+
+	const client = await auth.getClient();
+
+	const sheets = google.sheets({ version: 'v4', auth });
+
+	const SPREADSHEET_ID = "19Z1XH2btxOUfhZv_SSMcZQPqns7jBeyovdeXj66BtJA";
+
+	// READ data from a sheet
+	async function readSheet() {
+  	const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: SPREADSHEET_ID,
+    range: 'Sheet1!A1:D10', // Adjust range as needed
+  });
+
+  const rows = response.data.values;
+  console.log('Data:', rows);
+  return rows;
+
+	}
+	readSheet(); 
+})
+
+
+app.listen(3000, () => console.log("Server running on port 3000"));
